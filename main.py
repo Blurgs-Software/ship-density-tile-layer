c37f20ba77fb2ecc886b6b2a72e0d5c91f8316be
ansil-blurgs <ansil.md@blurgs.com>
2024-11-21 00:48:47 +0530

    changed docker file and port

--- main.py
+++ main.py
@@ -115,5 +115,6 @@ def get_tile(attribute, z, x, y):
         return "Tile not found", 404
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
+    
